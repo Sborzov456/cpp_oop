2cb3c5622b6e80a88213c1d7b371b33d0d4c19db
Alexandr Borzov <72516785+Sborzov456@users.noreply.github.com>
2021-12-07 02:24:58 +0300

    Add files via upload

---
+++ lab4/station with transition/station_with_transition_class.cpp
@@ -0,0 +1,13 @@
+#include "station_with_transition_class.h"
+void StationWithTransition::stationInformation() const
+{
+	cout << "Station Name: " << stationName << endl;
+	cout << "Station Type: Station With Transition" << endl;
+	cout << "Transition Lines: ";
+	if (transitionLines.empty()) cout << "Empty\n";
+	else {
+		for (unsigned int i = 0; i < MAX_T_LINES; i++)
+			if (!transitionLines[i].empty()) cout << transitionLines[i] << " ";
+		cout << endl;
+	}
+}
